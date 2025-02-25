176c11dea163b84c612fc0ab8deb616016926d26
fuad <nafisfuadisc@gmail.com>
2025-02-26 01:08:42 +0600

    JSON Update

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
 import 'package:splash_screen/home/account_page.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,8 +10,8 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       backgroundColor: const Color(0xFFF7F7F7),
-      splash: 'assets/squid.gif',
-      duration: 2000,
+      splash: LottieBuilder.asset('assets/squid-game-anim.json'),
+      duration: 1800,
       splashIconSize: 2000,
       nextScreen: const AccountManagePage(),
     );
